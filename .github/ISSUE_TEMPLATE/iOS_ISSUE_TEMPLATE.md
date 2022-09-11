9bc6ba91f58df40ee1582aad345df90217bbd8f5
heerucan <iwpow0307@gmail.com>
2022-09-11 23:01:34 +0900

    init: 책갈피 프로젝트 생성 #1

---
+++ .github/ISSUE_TEMPLATE/iOS_ISSUE_TEMPLATE.md
@@ -0,0 +1,15 @@
+---
+name: Bookmark Issue Template
+about: '책갈피 이슈 템플릿'
+title: '[]'
+labels: ''
+assignees: ''
+---
+
+### 이슈 설명
+
+
+
+### 체크 리스트
+
+- [ ] 
---
+++ Bookmark/Bookmark/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bookmark
+//
+//  Created by heerucan on 2022/09/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
