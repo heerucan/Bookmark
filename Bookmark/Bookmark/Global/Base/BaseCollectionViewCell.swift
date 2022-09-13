86c927e9b95e95f1f5512dd4d5829d05a8e8ce5e
heerucan <iwpow0307@gmail.com>
2022-09-13 19:24:58 +0900

    chore: @available(*, unavailable) 추가해서 런타임 에러 방지 #5

--- Bookmark/Bookmark/Global/Base/BaseCollectionViewCell.swift
+++ Bookmark/Bookmark/Global/Base/BaseCollectionViewCell.swift
@@ -20,6 +20,7 @@ class BaseCollectionViewCell: UICollectionViewCell {
         configureLayout()
     }
     
+    @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
--- Bookmark/Bookmark/Global/Base/BaseView.swift
+++ Bookmark/Bookmark/Global/Base/BaseView.swift
@@ -20,6 +20,7 @@ class BaseView: UIView {
         configureLayout()
     }
     
+    @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
