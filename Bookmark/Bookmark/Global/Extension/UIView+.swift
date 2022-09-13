1651f8bf002e71e30d3a833e0d437eac5de8d108
heerucan <iwpow0307@gmail.com>
2022-09-13 20:24:27 +0900

    chore: makeShadow Extension 추가 #2

--- Bookmark/Bookmark/Global/Extension/UIView+.swift
+++ Bookmark/Bookmark/Global/Extension/UIView+.swift
@@ -20,4 +20,15 @@ extension UIView {
         layer.cornerRadius = radius
         clipsToBounds = true
     }
+    
+//    shadowRadius : 블러의 정도 : 값이 커질수록 퍼짐
+//    shadowOffset : 그림자의 위치 - CGSize
+//    shadowOpacity : 그림자 모양 투명도 : 값이 커질수록 진해짐
+//    shadowPath : 그림자모양 (커스텀이 가능)
+    func makeShadow(radius: CGFloat, offset: CGSize, opacity: Float) {
+        layer.shadowColor = Color.black100.cgColor
+        layer.shadowRadius = radius
+        layer.shadowOffset = offset
+        layer.shadowOpacity = opacity
+    }
 }
