f4f31c4f73da5e8e65b5d7e5703b2683560f3996
heerucan <iwpow0307@gmail.com>
2022-09-16 13:11:06 +0900

    chore: UILabel Extension #2

--- Bookmark/Bookmark/Global/Extension/UILabel+.swift
+++ Bookmark/Bookmark/Global/Extension/UILabel+.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 extension UILabel {
-    func changeSearchTextColor(_ text: String?, _ keyword: String) {
+    func changeSearchTextColor(_ text: String?,
+                               _ keyword: String,
+                               color: UIColor = Color.green100) {
         guard let text = text else { return }
         let attributedText = NSMutableAttributedString(string: text)
         let range = NSString(string: text).range(of: keyword, options: .caseInsensitive)
-        let highlighteAttributes = [NSAttributedString.Key.foregroundColor: Color.green100]
+        let highlighteAttributes = [NSAttributedString.Key.foregroundColor: color]
         attributedText.addAttributes(highlighteAttributes, range: range)
         self.attributedText = attributedText
+    }
+    
+    func addLinkStyle(color: UIColor, range: String) {
+        if let labelText = text, labelText.count > 0 {
+            let attributedString = NSMutableAttributedString(string: labelText)
+            attributedString.addAttribute(.underlineColor, value: color, range: (labelText as NSString).range(of: range))
+            attributedString.addAttribute(.underlineStyle, value: NSUnderlineStyle.single.rawValue, range: (labelText as NSString).range(of: range))
+            attributedString.addAttribute(.foregroundColor, value: color, range: (labelText as NSString).range(of: range))
+            attributedText = attributedString
+        }
     }
 }
