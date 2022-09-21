3a9ee3e9eac507f3e686747fa845878346f542ae
heerucan <iwpow0307@gmail.com>
2022-09-22 06:56:48 +0900

    feat: 책갈피 꽂아두기 뷰 레이아웃 구현 완료 #13

--- Bookmark/Bookmark/Screen/Write/Controller/WriteViewController.swift
+++ Bookmark/Bookmark/Screen/Write/Controller/WriteViewController.swift
@@ -7,39 +7,141 @@
 
 import UIKit
 
+import PhotosUI
+
 final class WriteViewController: BaseViewController {
-    
-    // MARK: - Enum
-    
-    enum WriteViewType {
-        case sentence
-        case book
-    }
     
     // MARK: - Property
     
-    var viewType: WriteViewType = .sentence
+    let writeView = WriteView()
     
+    var fromWhatView: FromWhatViewType = .detail
     
     // MARK: - LifeCycle
     
+    override func loadView() {
+        self.view = writeView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setupAction()
     }
     
     // MARK: - Configure UI & Layout
     
     override func configureUI() {
         super.configureUI()
+//        writeView.descriptionLabel.text = writeView.writeViewState.description
     }
     
-    override func configureLayout() {
-        super.configureLayout()
+    override func setupDelegate() {
+        writeView.titleTextField.delegate = self
     }
     
     // MARK: - Custom Method
     
+    private func setupAction() {
+        writeView.imageButton.addTarget(self, action: #selector(touchupImageButton), for: .touchUpInside)
+        writeView.completeButton.addTarget(self, action: #selector(touchupCompleteButton(sender:)), for: .touchUpInside)
+        writeView.navigationView.rightBarButton.addTarget(self, action: #selector(touchupButton(_:)), for: .touchUpInside)
+        writeView.navigationView.backButton.addTarget(self, action: #selector(touchupButton(_:)), for: .touchUpInside)
+    }
     
+    private func setupPhotoPicker() {
+        var configuration = PHPickerConfiguration()
+        configuration.selectionLimit = 1
+        configuration.filter = .any(of: [.images])
+        let picker = PHPickerViewController(configuration: configuration)
+        picker.delegate = self
+        transition(picker, .present)
+        self.present(picker, animated: true)
+    }
+    
+    private func setupImagePicker() {
+        let picker = UIImagePickerController()
+        picker.sourceType = .camera
+        picker.allowsEditing = true
+        picker.mediaTypes = ["public.image"]
+        picker.delegate = self
+        self.present(picker, animated: true)
+    }
+        
     // MARK: - @objc
+    
+    @objc func touchupImageButton() {
+        let camera = UIAlertAction(title: "카메라 촬영", style: .default) { _ in
+            self.setupImagePicker()
+        }
+        let photo = UIAlertAction(title: "갤러리 선택", style: .default) { _ in
+            self.setupPhotoPicker()
+        }
+        showAlert(title: "갤러리/카메라 접근",
+                  message: nil,
+                  actions: [camera, photo],
+                  preferredStyle: .actionSheet)
+    }
+    
+    @objc func touchupCompleteButton(sender: UIButton) {
+        if fromWhatView == .detail {
+            transition(self, .pop)
+        } else if fromWhatView == .bookmark {
+            transition(self, .dismiss)
+        }
+    }
+    
+    @objc private func touchupButton(_ sender: UIButton) {
+        let ok = UIAlertAction(title: "확인", style: .destructive) { _ in
+            switch sender {
+            case self.writeView.navigationView.backButton:
+                return self.transition(self, .pop)
+            case self.writeView.navigationView.rightBarButton:
+                return self.transition(self, .dismiss)
+            default:
+                break
+            }
+        }
+        showAlert(title: Matrix.backPopupTitle,
+                  message: Matrix.backPopupMessage,
+                  actions: [ok],
+                  preferredStyle: .alert)
+    }
+}
+
+// MARK: - TextField Delegate
+
+extension WriteViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        writeView.titleTextField.resignFirstResponder()
+        return true
+    }
+}
+
+// MARK: - PHPicker / UIImagePicker / UINavigation Protocol
+
+extension WriteViewController: PHPickerViewControllerDelegate, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    // PHPickerViewControllerDelegate
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+        picker.dismiss(animated: true) {
+            if let itemProvider = results.first?.itemProvider,
+               itemProvider.canLoadObject(ofClass: UIImage.self) {
+                itemProvider.loadObject(ofClass: UIImage.self) { [weak self] (image, err) in
+                    DispatchQueue.main.async {
+                        guard let self = self else { return }
+                        self.writeView.image = image as? UIImage
+                    }
+                }
+            }
+        }
+    }
+    
+    // UIImagePickerControllerDelegate, UINavigationControllerDelegate
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else {
+            picker.transition(self, .dismiss)
+            return
+        }
+        self.writeView.image = image
+        picker.transition(self, .dismiss)
+    }
 }
--- Bookmark/Bookmark/Screen/Write/View/WriteView.swift
+++ Bookmark/Bookmark/Screen/Write/View/WriteView.swift
@@ -9,5 +9,103 @@ import UIKit
 
 final class WriteView: BaseView {
     
-
+    // MARK: - Property
+    
+    var image: UIImage? {
+        didSet {
+            imageButton.setImage(image, for: .normal)
+            completeButton.isDisabled = image != nil ? false : true
+        }
+    }
+    
+    var writeViewState: WriteViewState = .book {
+        didSet {
+            descriptionLabel.text = writeViewState.description
+            titleTextField.isHidden = writeViewState.isHidden
+        }
+    }
+    
+    let navigationView = BookmarkNavigationBar().then {
+        $0.titleLabel.text = "책갈피 꽂아두기"
+    }
+    
+    let descriptionLabel = UILabel().then {
+        $0.font = Font.body5.font
+        $0.textColor = Color.black100
+    }
+    
+    lazy var stackView = UIStackView(arrangedSubviews: [titleTextField, imageButton]).then {
+        $0.axis = .vertical
+        $0.distribution = .equalSpacing
+        $0.spacing = 20
+    }
+    
+    var titleTextField = BookmarkTextField().then {
+        $0.layer.cornerRadius = 5
+        $0.makeCornerStyle(width: 1, color: Color.gray300.cgColor, radius: 5)
+    }
+    
+    lazy var imageButton = UIButton().then {
+        $0.contentMode = .scaleAspectFill
+        $0.clipsToBounds = true
+        $0.makeCornerStyle(width: 1, color: Color.gray300.cgColor, radius: 5)
+        $0.addSubview(iconView)
+    }
+    
+    let iconView = UIImageView().then {
+        $0.image = Icon.Image.gallery
+    }
+    
+    let completeButton = BookmarkButton(.complete).then {
+        $0.isDisabled = true
+    }
+    
+    // MARK: - Initializer
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+  
+    // MARK: - Configure UI & Layout
+    
+    override func configureLayout() {
+        self.addSubviews([navigationView,
+                          descriptionLabel,
+                          stackView,
+                          completeButton])
+        
+        navigationView.snp.makeConstraints { make in
+            make.top.equalTo(self.safeAreaLayoutGuide)
+            make.directionalHorizontalEdges.equalToSuperview()
+            make.height.equalTo(57)
+        }
+        
+        descriptionLabel.snp.makeConstraints { make in
+            make.top.equalTo(navigationView.snp.bottom).offset(25)
+            make.centerX.equalToSuperview()
+        }
+        
+        stackView.snp.makeConstraints { make in
+            make.top.equalTo(descriptionLabel.snp.bottom).offset(25)
+            make.directionalHorizontalEdges.equalToSuperview().inset(16)
+        }
+        
+        titleTextField.snp.makeConstraints { make in
+            make.height.equalTo(43)
+        }
+        
+        imageButton.snp.makeConstraints { make in
+            make.height.equalTo(imageButton.snp.width).multipliedBy(1)
+        }
+        
+        iconView.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
+        
+        completeButton.snp.makeConstraints { make in
+            make.directionalHorizontalEdges.equalToSuperview().inset(16)
+            make.bottom.equalTo(self.safeAreaLayoutGuide.snp.bottom)
+            make.height.equalTo(49)
+        }
+    }
 }
