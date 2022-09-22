4e4996d25de6ea5089441520abc8b983ecf46015
heerucan <iwpow0307@gmail.com>
2022-09-22 13:56:12 +0900

    add: Store 스키마 설계 #17

--- Bookmark/Realm/RealmModel/Record.swift
+++ Bookmark/Realm/RealmModel/Record.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+
+import RealmSwift
+
+// MARK: - Record
+
+class Record: Object {
+    @Persisted var store: Store
+    @Persisted var title: String?
+    @Persisted var image: String
+    @Persisted var category: Bool
+    @Persisted var createdAt = Date()
+    
+    @Persisted(primaryKey: true) var objectId: ObjectId
+    
+    convenience init(store: Store,
+                     title: String?,
+                     image: String,
+                     category: Bool,
+                     createdAt: Date) {
+        self.init()
+        self.store = Store()
+        self.title = title
+        self.image = image
+        self.category = category
+        self.createdAt = createdAt
+    }
+}
