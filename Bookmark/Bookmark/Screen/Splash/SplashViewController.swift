9e8f5895084c6d6c1f7447cb3033287f13536b1d
heerucan <iwpow0307@gmail.com>
2022-09-28 12:25:45 +0900

    feat: splash 구현 #19

---
+++ Bookmark/Bookmark/Screen/Splash/SplashViewController.swift
@@ -0,0 +1,49 @@
+//
+//  SplashViewController.swift
+//  Bookmark
+//
+//  Created by heerucan on 2022/09/28.
+//
+
+import UIKit
+
+final class SplashViewController: BaseViewController {
+    
+    // MARK: - Property
+    
+    private let iconImage = UIImageView().then {
+        $0.image = Icon.Logo.white
+        $0.alpha = 0
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureAnimation()
+    }
+    
+    override func configureUI() {
+        view.backgroundColor = Color.gray100
+    }
+    
+    override func configureLayout() {
+        view.addSubview(iconImage)
+        
+        iconImage.snp.makeConstraints { make in
+            make.leading.equalToSuperview().inset(35)
+            make.bottom.equalTo(view.safeAreaLayoutGuide).inset(50)
+        }
+    }
+    
+    // MARK: - Custom Method
+    
+    private func configureAnimation() {
+        UIView.animate(withDuration: 0.2, delay: 0.2) {
+            self.iconImage.image = Icon.Logo.red
+            self.iconImage.alpha = 1
+        } completion: { finished in
+            let viewController = UINavigationController(rootViewController: TabBarViewController())
+            viewController.modalPresentationStyle = .fullScreen
+            self.present(viewController, animated: false, completion: nil)
+        }
+    }
+}
