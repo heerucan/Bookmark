4be06829f9c8aa2ded26bac8bcb492166d45211a
heerucan <jwyandkrh@naver.com>
2022-10-19 16:22:52 +0900

    chore: noti에서 badge 빼기 #29'

--- Bookmark/Bookmark/Support/AppDelegate.swift
+++ Bookmark/Bookmark/Support/AppDelegate.swift
@@ -25,14 +25,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             // For iOS 10 display notification (sent via APNS)
             UNUserNotificationCenter.current().delegate = self
             
-            let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+            let authOptions: UNAuthorizationOptions = [.alert, .sound]
             UNUserNotificationCenter.current().requestAuthorization(
                 options: authOptions,
                 completionHandler: { _, _ in }
             )
         } else {
             let settings: UIUserNotificationSettings =
-            UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+            UIUserNotificationSettings(types: [.alert, .sound], categories: nil)
             application.registerUserNotificationSettings(settings)
         }
         
