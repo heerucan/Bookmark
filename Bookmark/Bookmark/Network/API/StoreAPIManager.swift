2755077de120186298dc2d06775d3867dd33bb12
heerucan <iwpow0307@gmail.com>
2022-10-23 19:52:19 +0900

    chore: StoreAPIManager 코드 변경 #15

--- Bookmark/Bookmark/Network/API/StoreAPIManager.swift
+++ Bookmark/Bookmark/Network/API/StoreAPIManager.swift
@@ -7,44 +7,25 @@
 
 import Foundation
 
+import Alamofire
+
 final class StoreAPIManager {
     private init() { }
     static let shared = StoreAPIManager()
     
-    func fetchBookStore(completion: @escaping (BookStore?, APIError?) -> Void) {
-        guard let url = EndPoint.seoul.makeURL() else { return }       
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            guard error == nil else {
-                print("요청 실패")
-                completion(nil, .failedRequest)
-                return
+    typealias completion = ((BookStore?, Int?, Error?) -> Void)
+    
+    // MARK: - GET : BookStore
+    
+    func fetchBookStore(completion: @escaping completion) {
+        AF.request(StoreRouter.bootStore).validate(statusCode: 200..<500).responseDecodable(of: BookStore.self) { response in
+            let statusCode = response.response?.statusCode
+            switch response.result {
+            case .success(let value):
+                completion(value, statusCode, nil)
+            case .failure(let error):
+                completion(nil, statusCode, error)
             }
-
-            guard let data = data else {
-                print("반환된 데이터 없음")
-                completion(nil, .noData)
-                return
-            }
-
-            guard let response = response as? HTTPURLResponse else {
-                print("잘못된 응답")
-                completion(nil, .invalidResponse)
-                return
-            }
-            
-            guard (200..<300).contains(response.statusCode) else {
-                print("응답 실패")
-                completion(nil, .failedRequest)
-                return
-            }
-            
-            do {
-                let result = try JSONDecoder().decode(BookStore.self, from: data)
-                completion(result, nil)
-            } catch {
-                print(error)
-                completion(nil, .invalidData)
-            }
-        }.resume()
+        }
     }
 }
---
+++ Bookmark/Bookmark/Network/Base/APIConstant.swift
@@ -0,0 +1,8 @@
+//
+//  APIConstant.swift
+//  Bookmark
+//
+//  Created by heerucan on 2022/10/23.
+//
+
+import Foundation
