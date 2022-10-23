ea3ee7b3ac422e5d9270d79958d939abd74650d7
heerucan <iwpow0307@gmail.com>
2022-10-23 19:52:38 +0900

    add: StoreRouter 추가 #15

--- Bookmark/Bookmark/Network/API/StoreRouter.swift
+++ Bookmark/Bookmark/Network/API/StoreRouter.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+
+import Alamofire
+
+@frozen
+enum StoreRouter {
+    case bootStore
+}
+
+extension StoreRouter: URLRequestConvertible {
+    private var baseURL: URL {
+        return URL(string: APIConstant.baseURL)!
+    }
+    
+    private var method: HTTPMethod {
+        switch self {
+        case .bootStore:
+            return .get
+        }
+    }
+    
+    private var path: String {
+        switch self {
+        case .bootStore:
+            // MARK: - FIX endIndex로 변경
+            return "/\(APIKey.bookStoreAccessKey)/json/TbSlibBookstoreInfo/1/613"
+        }
+    }
+    
+    func asURLRequest() throws -> URLRequest {
+        let url = baseURL.appendingPathComponent(path)
+        var request = URLRequest(url: url)
+        request.method = method
+        return request
+    }
+}
