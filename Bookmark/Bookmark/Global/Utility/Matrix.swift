ef4ea10b6ad16595d18fde77e7a1834f6e2a5056
heerucan <iwpow0307@gmail.com>
2022-10-30 19:55:01 +0900

    chore: 책 한 권 문구 변경 #38

--- Bookmark/Bookmark/Global/Utility/Matrix.swift
+++ Bookmark/Bookmark/Global/Utility/Matrix.swift
@@ -25,12 +25,13 @@ enum Matrix {
     static let new = "0002"
     static let old = "0001"
     
-    // 팝업 메시지
+    // 팝업 메시지 & 문자열
     static let settingMessage = "위치 서비스를 사용할 수 없습니다. 기기의 '설정>개인정보 보호'에서 위치 서비스를 켜주세요."
     static let backPopupTitle = "완료하지 않고 나갈 건가요?"
     static let backPopupMessage = "기록한 책갈피는 저장되지 않아요!"
     static let sentence = "공감 가는 글을 꽂아두세요"
-    static let book = "사고 싶은 책을 꽂아두세요"
+    static let book = "갖고 싶은 책을 꽂아두세요"
     static let textFieldPlaceholder = "책 제목은 필수가 아니에요 :)"
     static let clipboard = "클립보드에 책방 주소가 복사됐어요!"
+    static let writeInfoText = "* 이미지는 추후 수정하실 수 없어요."
 }
