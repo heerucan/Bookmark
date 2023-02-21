93b49992a08dd19693dca81515748729cee38186
heerucan <iwpow0307@gmail.com>
2023-02-21 14:00:24 +0900

    chore: ci_pre_xcodebuild.sh 수정

--- Bookmark/ci_scripts/ci_pre_xcodebuild.sh
+++ Bookmark/ci_scripts/ci_pre_xcodebuild.sh
@@ -3,9 +3,4 @@
 #
 #
 echo "BOOKSTORE_API_KEY = $BOOKSTORE_API_KEY" > "$CI_WORKSPACE/Secrets.xcconfig"
-echo "ASK_NOTION = $ASK_NOTION" > "$CI_WORKSPACE/Secrets.xcconfig"
-echo "MY_APP_ID = $MY_APP_ID" > "$CI_WORKSPACE/Secrets.xcconfig"
-echo "INTRODUCE_NOTION = $INTRODUCE_NOTION" > "$CI_WORKSPACE/Secrets.xcconfig"
-echo "CLIENT_ID = $CLIENT_ID" > "$CI_WORKSPACE/Secrets.xcconfig"
-echo "NOTION = $NOTION" > "$CI_WORKSPACE/Secrets.xcconfig"
 echo "BUNDLE_NAME = $BUNDLE_NAME" > "$CI_WORKSPACE/Secrets.xcconfig"
